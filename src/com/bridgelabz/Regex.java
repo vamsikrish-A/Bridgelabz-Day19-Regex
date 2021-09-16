0324d42f7851e869bd585ddc378492571a7bbe74
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-16 22:38:54 +0530

    valid lastname

--- src/com/bridgelabz/Regex.java
+++ src/com/bridgelabz/Regex.java
@@ -14,8 +14,15 @@ public class Regex {
         System.out.println("Welcome to Regex.");
 
         //uSing Quantifiers for first name
+        System.out.println("Valid Firstname ");
         System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","Vams"));  //True , first name starts with cap and has minimum 3 characters.
         System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","Vamsi")); //false, has more characters.
         System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","vams"));  //false, has no cap and more characters.
+
+        //uSing Quantifiers for last name
+        System.out.println("Valid Lastname ");
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","Kris"));  //True , first name starts with cap and has minimum 3 characters.
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","Krishna")); //false, has more characters.
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","krishna"));
     }
 }
